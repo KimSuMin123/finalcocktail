b9d52c355e1903a6b4948b31fe1dd080a94d73e7
Kimsumin123 <tntn211@naver.com>
2023-06-19 13:57:52 +0900

    sumin

--- src/pages/RandomPick.jsx
+++ src/pages/RandomPick.jsx
@@ -1,4 +1,6 @@
 import Header from "./Header";
+import Logo from "../img/logo.png";
+import '../css/RandomPick.css'
 
 function RandomPick() {
     const cocktail = ["1번 칵테일", "2번 칵테일","3번 칵테일", "4번 칵테일", "5번 칵테일", "6번 칵테일"]
@@ -8,8 +10,11 @@ function RandomPick() {
   return (
     <div>
       <Header/>
-      <h1>칵테일 랜덤 추천</h1>
-      {cocktail[getRamdomIndex(cocktail.length)]}
+      <div id = "RandomPick">
+        <h1>칵테일 랜덤 추천</h1>
+        <img src={Logo} width={200}/><br/>
+        {cocktail[getRamdomIndex(cocktail.length)]}
+      </div>
     </div>
   );
 }
